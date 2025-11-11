455dac5a3b864890de7456a95a8d26a67b0479b3
Никита <ngopskyy@gmail.com>
2025-11-11 22:06:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-$(function() {
-    let i = 0
-    const hero = $('.hero');
-    const imageUrls = [
-        'url(./img/hero/hero-background1.png)',
-        'url(./img/hero/hero-background2.png)',
-        'url(./img/hero/hero-background3.png)'
-    ]
+let i = 0
+const hero = document.querySelector('.hero');
+const imageUrls = [
+    'url(./img/hero/hero-background1.png)',
+    'url(./img/hero/hero-background2.png)',
+    'url(./img/hero/hero-background3.png)'
+]
 
-    function changeBackground() {
-        hero.css('background-image', imageUrls[i])
-        i = (i + 1) % imageUrls.length;
-    }
+function changeBackground() {
+    hero.style.backgroundImage = imageUrls[i];
+    i = (i + 1) % imageUrls.length;
+}
 
-    setInterval(changeBackground, 3000)
+changeBackground();
 
-    changeBackground();
-});
+setInterval(changeBackground, 3000)
